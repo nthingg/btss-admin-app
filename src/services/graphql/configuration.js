ebf02d1fba166c15c82e9d4f58d807558d9fa7b5
nameis-duy <dphan2412@gmail.com>
2024-06-09 01:40:37 +0700

    DuyPD - update config page

--- src/services/graphql/configuration.js
+++ src/services/graphql/configuration.js
@@ -17,6 +17,13 @@ export const LOAD_SYSTEM_CONFIGURATIONS = gql`
             PRODUCT_MAX_PRICE_UP_PCT
             PLAN_COMPLETE_AFTER_DAYS
             ORDER_COMPLETE_AFTER_DAYS
+            MIN_PLAN_MEMBER
+            MAX_PLAN_MEMBER
+            MIN_DEPART_DIFF
+            MAX_DEPART_DIFF
+            MIN_PERIOD
+            MAX_PERIOD
+            BUDGET_ASSURANCE_RATE
             HOLIDAYS {
                 name
                 from
